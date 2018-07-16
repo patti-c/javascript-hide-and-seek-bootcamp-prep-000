983291a4c797a6140d9d89b8847a7a6f6e30095b
Patricia Carter <patricia4194@gmail.com>
2018-07-16 15:28:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget(target) {
+  return document.getElementById('nested').querySelector('.target');
+}
+
+function increaseRankBy(n) {
+  var rankedList = document.querySelectorAll('.ranked-list');
+  for(let i = 0 ; i < rankedList.length ; i++) {
+    rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  
+}
